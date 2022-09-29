1c6b6f43fb2dbe4d1d909640e1984646102e13ef
Anna Hermes <anna.hermes@gmail.com>
2022-09-29 18:16:20 -0300

    code improvements

--- anna_cl.py
+++ anna_cl.py
@@ -10,6 +10,7 @@ MENU
 2 - Listar todos alunos
 Escolha: """)
 
+# entrada de dados
     if esc == "1":
         nome = input("Nome: ")
         lalunos.append(nome)
@@ -19,6 +20,7 @@ Escolha: """)
         ldata.append(data)
         print(f'\n1{nome} - {matricula} - {data}')
 
+# print da lista de alunos cadastrados
     if esc == "2":
         for cont in range (len(lalunos)):
             print(f'{cont+1}. {lalunos[cont]} - {lmatricula[cont]} - {ldata[cont]}')
